e656279df40c2a2caf52d1330df511c80fdc4cbd
AlexiWolf <alexiwolf@pm.me>
2022-04-17 11:46:01 -0500

    Moved contexts to their own modules

---
+++ src/context.rs
@@ -0,0 +1,6 @@
+use wolf_engine::*;
+
+pub struct SdlContext;
+
+impl Subcontext for SdlContext {}
+
---
+++ src/video_context.rs
@@ -0,0 +1,6 @@
+use wolf_engine::*;
+
+pub struct SdlVideoContext;
+
+impl Subcontext for SdlVideoContext {}
+
