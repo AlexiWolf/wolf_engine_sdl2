a075898f4a75d611fb5eb8dbbf6f275ba83469c2
AlexiWolf <alexiwolf@pm.me>
2022-04-17 11:25:16 -0500

    Created test with usage of SdlPlugin

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,11 @@ pub use sdl2;
 
 #[cfg(test)]
 mod sdl2_plugin_tests {
+    use wolf_engine::EngineBuilder;
+
     #[test]
-    fn should_work() {
-        assert!(true, "Hello, World!");
+    fn should_have_sdl_plugin() {
+        let engine_builder = EngineBuilder::new();
+        let sdl_plugin = SdlPlugin::new();
     }
 }
