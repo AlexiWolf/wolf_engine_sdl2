6754f4881455f68e56c38c458446ccc9af11d5e6
AlexiWolf <alexiwolf@pm.me>
2022-04-17 11:38:27 -0500

    Loaded SdlVideoContext

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,9 @@ impl SdlPlugin {
 
 impl Plugin for SdlPlugin {
     fn setup(&mut self, engine_builder: EngineBuilder) -> PluginResult {
-        Ok(engine_builder.with_subcontext(SdlContext))
+        Ok(engine_builder
+            .with_subcontext(SdlContext)
+            .with_subcontext(SdlVideoContext))
     }
 }
 
