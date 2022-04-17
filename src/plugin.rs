e5605d2bb29e4d4dff93290cff6868d68e763eb0
AlexiWolf <alexiwolf@pm.me>
2022-04-17 11:44:45 -0500

    Moved SdlPlugin and tests to the plugin module

---
+++ src/plugin.rs
@@ -0,0 +1,35 @@
+pub use crate::*;
+pub use wolf_engine::*;
+
+pub struct SdlPlugin;
+
+impl SdlPlugin {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl Plugin for SdlPlugin {
+    fn setup(&mut self, engine_builder: EngineBuilder) -> PluginResult {
+        Ok(engine_builder
+            .with_subcontext(SdlContext)
+            .with_subcontext(SdlVideoContext))
+    }
+}
+
+#[cfg(test)]
+mod sdl2_plugin_tests {
+    use super::*;
+
+    #[test]
+    fn should_have_sdl_plugin() {
+        let mut engine_builder = EngineBuilder::new();
+        let sdl_plugin = SdlPlugin::new();
+
+        engine_builder = engine_builder.with_plugin(Box::from(sdl_plugin)); 
+        let engine = engine_builder.build();
+
+        assert!(engine.context.borrow::<SdlContext>().is_some());
+        assert!(engine.context.borrow::<SdlVideoContext>().is_some());
+    }
+}
