f2370f8a32810aa57e74e946cc951c8daee51680
AlexiWolf <alexiwolf@pm.me>
2022-04-18 18:48:29 -0500

    Stubbed SdlWindowSettings argument

--- src/video_context.rs
+++ src/video_context.rs
@@ -11,7 +11,7 @@ use wolf_engine::*;
 pub struct SdlVideoContext;
 
 impl SdlVideoContext {
-    pub fn new() -> Self {
+    pub fn new(window_settings: SdlWindowSettings) -> Self {
         Self
     }
 }
