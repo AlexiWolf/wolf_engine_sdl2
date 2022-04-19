be46711db9eae8273be91466185cc633c50cd637
AlexiWolf <alexiwolf@pm.me>
2022-04-18 22:39:22 -0500

    Implemented quickstart example

--- examples/quickstart/src/main.rs
+++ examples/quickstart/src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use log::*;
+use wolf_engine::*;
+use wolf_engine_sdl2::*;
+
+pub fn main() {
+    logging::initialize_logging(LevelFilter::Debug);
+
+    EngineBuilder::new()
+        .with_plugin(Box::from(SdlPlugin::new(SdlWindowSettings::default())))
+        .build()
+        .run(Box::from(ExampleGame::new()));
+}
+
+pub struct ExampleGame;
+
+impl ExampleGame {
+    pub fn new() -> Self {
+        Self
+    }
+}
+
+impl State for ExampleGame {
+    fn update(&mut self, _context: &mut Context) -> OptionalTransition {
+        None
+    }
+
+    fn render(&mut self, context: &mut Context) -> RenderResult {
+        
+    }
 }
