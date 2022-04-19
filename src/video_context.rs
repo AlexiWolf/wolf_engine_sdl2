9dc0d2c114dc5d2cb77d99e4a9f3edd57773b2a7
AlexiWolf <alexiwolf@pm.me>
2022-04-18 19:00:18 -0500

    Updated the documentation

--- src/video_context.rs
+++ src/video_context.rs
@@ -3,11 +3,22 @@ use wolf_engine::*;
 /// Provides access to SDL's [Window Canvas](Canvas) and [VideoSubsystem]. 
 ///
 /// # Examples
-///
+/// 
+/// The Video Context allows you to access SDL's [VideoSubsystem] and [Canvas].
 /// ```
 /// # use wolf_engine_sdl2::*;
 /// # let sdl_video_context = SdlVideoContext::new(SdlWindowSettings::default());
+/// // Get immutable access to the video subsystem and canvas.
+/// let video_subsystem = sdl_video_context.video();
+/// let window_canvas = sdl_video_context.canvas();
+/// # drop(video_subsystem);
+/// # drop(window_canvas);
+/// // Get mutable access to the video subsystem and canvas.
+/// let video_subsystem_mut = sdl_video_context.video_mut();
+/// let window_canvas = sdl_video_context.canvas_mut();
 /// ```
+///
+/// From there, you can use SDL as you would normally.
 pub struct SdlVideoContext;
 
 impl SdlVideoContext {
