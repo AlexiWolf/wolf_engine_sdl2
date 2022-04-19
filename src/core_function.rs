dbf6ee735eb89725f12562521ed6a5aab5f8d925
AlexiWolf <alexiwolf@pm.me>
2022-04-18 22:25:15 -0500

    Implemented base engine logic

--- src/core_function.rs
+++ src/core_function.rs
@@ -2,5 +2,12 @@ use crate::*;
 use wolf_engine::*;
 
 pub fn run_with_sdl(mut engine: Engine) {
-    
+    while engine.state_stack.is_not_empty() {
+        engine 
+            .scheduler
+            .update(&mut engine.context, &mut engine.state_stack);
+        engine
+            .scheduler
+            .render(&mut engine.context, &mut engine.state_stack);
+    }
 }
