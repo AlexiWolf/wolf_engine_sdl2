a8f2dd1473353c00577d152d5077db5ba2c9aee1
AlexiWolf <alexiwolf@pm.me>
2022-05-19 15:55:42 -0500

    Added initialization for the audio subsystem

--- src/audio_context.rs
+++ src/audio_context.rs
@@ -1,5 +1,16 @@
+use sdl2::{AudioSubsystem, Sdl};
 use wolf_engine::*;
 
-pub struct SdlAudioContext {}
+pub struct SdlAudioContext {
+    pub subsystem: AudioSubsystem,
+}
+
+impl SdlAudioContext {
+    pub fn new(sdl: &Sdl) -> Self {
+        Self {
+            subsystem: sdl.audio().expect("Failed to initialize the audio subsystem"),
+        }
+    }
+}
 
 impl Subcontext for SdlAudioContext {}
