44c903972eb03bfc44a53f834ecdd5649157b502
AlexiWolf <alexiwolf@pm.me>
2022-05-22 16:19:13 -0500

    Added mixer setup

--- src/mixer_context.rs
+++ src/mixer_context.rs
@@ -1,10 +1,16 @@
 use wolf_engine::*;
+use sdl2::mixer;
 
-pub struct SdlMixerContext {}
+pub struct SdlMixerContext {
+    pub subsystem: mixer::Sdl2MixerContext,
+}
 
 impl SdlMixerContext {
     pub fn new() -> Self {
-        Self {}
+        Self {
+            subsystem: mixer::init(mixer::InitFlag::all())
+                .expect("Failed to initialize mixer"),
+        }
     }
 }
 
