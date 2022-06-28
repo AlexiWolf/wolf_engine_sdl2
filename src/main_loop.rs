e88d3f7e4ce1c4849ca785e5c2d4ae32c9fb7a54
AlexiWolf <alexiwolf@pm.me>
2022-06-28 13:07:33 -0500

    Changed `run_with_sdl` to a `MainLoop` impl

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -3,30 +3,38 @@ use sdl2::event::Event;
 use wolf_engine::utils::*;
 use wolf_engine::*;
 
-pub(crate) fn run_with_sdl(mut engine: Engine) {
-    log_sdl_version();
-    let mut has_window_quit = false;
-    while engine.state_stack.is_not_empty() {
-        profile_new_frame();
-        profile_scope!("frame");
-        if let Some(Ok(sdl_context)) = engine.context.try_borrow::<SdlContext>() {
-            profile_scope!("process_sdl_events");
-            let mut event_pump = sdl_context.sdl.event_pump().unwrap();
-            for event in event_pump.poll_iter() {
-                if let Event::Quit { .. } = event {
-                    has_window_quit = true;
+pub struct Sdl2MainLoop;
+
+impl MainLoop for Sdl2MainLoop {
+    fn run(&mut self, engine: Engine) -> Engine {
+        log_sdl_version();
+        let mut has_window_quit = false;
+
+        while engine.state_stack.is_not_empty() {
+            profile_new_frame();
+            profile_scope!("frame");
+            {
+                let sdl_context = engine.context.borrow::<SdlContext>().expect("No SdlContext");
+                profile_scope!("process_sdl_events");
+                let mut event_pump = sdl_context.sdl.event_pump().unwrap();
+                for event in event_pump.poll_iter() {
+                    if let Event::Quit { .. } = event {
+                        has_window_quit = true;
+                    }
                 }
+            }
+            engine
+                .scheduler
+                .update(&mut engine.context, &mut engine.state_stack);
+            engine
+                .scheduler
+                .render(&mut engine.context, &mut engine.state_stack);
+            if has_window_quit {
+                log::debug!("The SDL window has quit.  Shutting down the engine.");
+                engine.state_stack.clear(&mut engine.context);
             }
         }
         engine
-            .scheduler
-            .update(&mut engine.context, &mut engine.state_stack);
-        engine
-            .scheduler
-            .render(&mut engine.context, &mut engine.state_stack);
-        if has_window_quit {
-            log::debug!("The SDL window has quit.  Shutting down the engine.");
-            engine.state_stack.clear(&mut engine.context);
-        }
     }
 }
+
