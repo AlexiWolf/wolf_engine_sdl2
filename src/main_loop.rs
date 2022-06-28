6ff99a13e8d48c4df83c5b67d9a351d77cd999a4
AlexiWolf <alexiwolf@pm.me>
2022-06-28 14:11:10 -0500

    Moved `has_window_quit` to struct

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -3,18 +3,21 @@ use sdl2::event::Event;
 use wolf_engine::utils::*;
 use wolf_engine::*;
 
-pub struct SdlMainLoop;
+pub struct SdlMainLoop {
+    has_window_quit: bool,
+}
 
 impl SdlMainLoop {
     pub fn new() -> Self {
-        Self
+        Self {
+            has_window_quit: false,
+        }
     }
 }
 
 impl MainLoop for SdlMainLoop {
     fn run(&mut self, mut engine: Engine) -> Engine {
         log_sdl_version();
-        let mut has_window_quit = false;
 
         while engine.state_stack.is_not_empty() {
             profile_new_frame();
@@ -25,7 +28,7 @@ impl MainLoop for SdlMainLoop {
                 let mut event_pump = sdl_context.sdl.event_pump().unwrap();
                 for event in event_pump.poll_iter() {
                     if let Event::Quit { .. } = event {
-                        has_window_quit = true;
+                        self.has_window_quit = true;
                     }
                 }
             }
@@ -35,7 +38,7 @@ impl MainLoop for SdlMainLoop {
             engine
                 .scheduler
                 .render(&mut engine.context, &mut engine.state_stack);
-            if has_window_quit {
+            if self.has_window_quit {
                 log::debug!("The SDL window has quit.  Shutting down the engine.");
                 engine.state_stack.clear(&mut engine.context);
             }
