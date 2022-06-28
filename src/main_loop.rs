4fa6d90b85b2de6d554b2487509b18fc26614835
AlexiWolf <alexiwolf@pm.me>
2022-06-28 14:16:27 -0500

    Moved event processing code to its own function

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -13,6 +13,17 @@ impl SdlMainLoop {
             has_window_quit: false,
         }
     }
+
+    fn process_sdl_events(&mut self, context: &Context) {
+        profile_scope!("process_sdl_events");
+        let sdl_context = context.borrow::<SdlContext>().expect("No SdlContext");
+        let mut event_pump = sdl_context.sdl.event_pump().unwrap();
+        for event in event_pump.poll_iter() {
+            if let Event::Quit { .. } = event {
+                self.has_window_quit = true;
+            }
+        }
+    }
 }
 
 impl MainLoop for SdlMainLoop {
@@ -22,22 +33,15 @@ impl MainLoop for SdlMainLoop {
         while engine.state_stack.is_not_empty() {
             profile_new_frame();
             profile_scope!("frame");
-            {
-                let sdl_context = engine.context.borrow::<SdlContext>().expect("No SdlContext");
-                profile_scope!("process_sdl_events");
-                let mut event_pump = sdl_context.sdl.event_pump().unwrap();
-                for event in event_pump.poll_iter() {
-                    if let Event::Quit { .. } = event {
-                        self.has_window_quit = true;
-                    }
-                }
-            }
+            self.process_sdl_events(&engine.context);
+
             engine
                 .scheduler
                 .update(&mut engine.context, &mut engine.state_stack);
             engine
                 .scheduler
                 .render(&mut engine.context, &mut engine.state_stack);
+
             if self.has_window_quit {
                 log::debug!("The SDL window has quit.  Shutting down the engine.");
                 engine.quit();
