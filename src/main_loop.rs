b801bb50fde475f9fb331fbd65ec4de8238c592e
AlexiWolf <alexiwolf@pm.me>
2022-08-20 10:18:43 -0500

    Updated `SdlMainLoop` to use new `Engine::has_quit()` method

--- src/main_loop.rs
+++ src/main_loop.rs
@@ -10,7 +10,7 @@ pub struct SdlMainLoop {
 impl MainLoop for SdlMainLoop {
     fn run(&mut self, mut engine: Engine) -> Engine {
         log_sdl_version();
-        while engine.state_stack.is_not_empty() {
+        while !engine.has_quit() {
             profile_new_frame!();
             profile_scope!("frame");
             self.process_sdl_events(&engine.context);
