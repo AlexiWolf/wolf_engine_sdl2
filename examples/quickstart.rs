29104d39535ebcabd6df71facee6c601a09a0150
AlexiWolf <alexiwolf@pm.me>
2022-08-24 21:15:09 -0500

    Added spinning text to the center of the quickstart demo screen

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -1,6 +1,9 @@
 use colors_transform::{Color, Hsl};
 use log::*;
-use sdl2::{gfx::primitives::DrawRenderer, pixels::Color as SdlColor};
+use sdl2::{
+    gfx::primitives::DrawRenderer, pixels::Color as SdlColor, pixels::PixelFormatEnum, rect::{Rect, Point},
+    render::{Canvas, Texture}, surface::Surface,
+};
 use wolf_engine::*;
 use wolf_engine_sdl2::*;
 
@@ -20,32 +23,57 @@ pub fn main() {
 
 pub struct ExampleGame {
     hew: f32,
+    rotation: f64,
 }
 
 impl ExampleGame {
     pub fn new() -> Self {
-        Self { hew: 0.0 }
+        Self { 
+            hew: 0.0, 
+            rotation: 0.0, 
+        }
     }
 }
 
 impl State for ExampleGame {
     fn update(&mut self, _context: &mut Context) -> OptionalTransition {
         self.hew = (self.hew + 1.0) % 360.0;
+        self.rotation = (self.rotation + 1.0) % 360.0;
         None
     }
 
     fn render(&mut self, context: &mut Context) -> RenderResult {
-        let mut sdl = context.borrow_mut::<SdlVideoContext>().unwrap();
+        let mut video = context.borrow_mut::<SdlVideoContext>().unwrap();
         let color = Hsl::from(self.hew, 100.0, 50.0);
-        sdl.canvas.set_draw_color(SdlColor::RGB(
+        video.canvas.set_draw_color(SdlColor::RGB(
             color.get_red() as u8,
             color.get_green() as u8,
             color.get_blue() as u8,
         ));
-        sdl.canvas.clear();
-        sdl.canvas
-            .string(10, 10, "Hello, SDL2!", SdlColor::BLACK)
+        video.canvas.clear();
+
+        let texture_creator = video.canvas.texture_creator();
+        let mut texture = texture_creator
+            .create_texture_target(PixelFormatEnum::RGBA8888, 100, 10)
             .unwrap();
-        sdl.canvas.present();
+        video.canvas.with_texture_canvas(&mut texture, |texture_canvas| {
+            texture_canvas
+                .string(3, 1, "Hello, SDL2!", SdlColor::WHITE)
+                .unwrap();
+        }).unwrap();
+
+        let (width, height) = video.canvas.window().drawable_size();
+
+
+        video.canvas.copy_ex(
+            &texture,
+            Rect::new(0, 0, 100, 10), 
+            Rect::new((width as i32 / 2) - 50, (height as i32 / 2) - 5, 100, 10), 
+            self.rotation, 
+            None,
+            false, 
+            false,
+        ).unwrap();
+        video.canvas.present();
     }
 }
