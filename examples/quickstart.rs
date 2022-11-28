7192f25f9c55523d9d09e57e9e8f922829de59ee
AlexiWolf <alexiwolf@pm.me>
2022-11-28 11:23:35 -0600

    Added main loop to quickstart example

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -3,5 +3,17 @@ use wolf_engine::prelude::*;
 pub fn main() {
     let sdl2 = wolf_engine_sdl2::init()
         .expect("Failed to initialize SDL2");
-    let engine = Engine::from(sdl2); 
+    let mut engine = Engine::from(sdl2); 
+
+    while let Some(event) = engine.next_event() {
+         match event {
+            Event::Quit => println!("Goodbye!"),
+            Event::Update => (), 
+            Event::Render => (),
+            Event::EventsCleared => {
+                engine.update();
+                engine.render();
+            },
+        }
+    }
 }
