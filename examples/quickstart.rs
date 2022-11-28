a32d09d8539f8bfd72d9ad0b9758638a7b79d934
AlexiWolf <alexiwolf@pm.me>
2022-11-28 16:31:33 -0600

    Added window creation code

--- examples/quickstart.rs
+++ examples/quickstart.rs
@@ -4,6 +4,11 @@ pub fn main() {
     let sdl2 = wolf_engine_sdl2::init()
         .expect("Failed to initialize SDL2");
     let mut engine = Engine::from(sdl2); 
+    let window = engine.context()
+            .window("Wolf Engine / SDL2 Quickstart Demo", 800, 600)
+            .position_centered()
+            .build()
+            .expect("Failed to create the window");
 
     while let Some(event) = engine.next_event() {
         engine.context_mut().handle_events();
---
+++ tests/sdl_context_tests.rs
@@ -0,0 +1,5 @@
+#[test]
+pub fn should_initialize_sdl() {
+    let sdl_context = wolf_engine_sdl2::init();
+    
+}
