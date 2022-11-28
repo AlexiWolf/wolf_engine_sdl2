6c6f4a78a00694a26db15387ee718483ddb20677
AlexiWolf <alexiwolf@pm.me>
2022-11-28 11:20:38 -0600

    Implemented `EventLoop` for `SdlContext`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 use wolf_engine::events::Event as WolfEvent;
 use wolf_engine::events::EventLoop as WolfEventLoop;
+use wolf_engine::events::EventQueue as WolfEventQueue;
 
 pub fn init() -> Result<SdlContext, String> {
     let sdl_context = SdlContext::new();
@@ -7,19 +8,21 @@ pub fn init() -> Result<SdlContext, String> {
 }
 
 pub struct SdlContext {
-    
+    wolf_event_queue: WolfEventQueue<WolfEvent>, 
 }
 
 impl SdlContext {
     pub fn new() -> Self {
-        Self { }
+        Self { wolf_event_queue: WolfEventQueue::new() }
     }
 }
 
 impl WolfEventLoop<WolfEvent> for SdlContext {
     fn next_event(&mut self) -> Option<WolfEvent> {
-        None
+        self.wolf_event_queue.next_event()
     }
 
-    fn send_event(&self, event: WolfEvent) {}
+    fn send_event(&self, event: WolfEvent) {
+        self.wolf_event_queue.send_event(event)
+    }
 }
