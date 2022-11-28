42c131f821d90430b62f35baaa40071618430f10
AlexiWolf <alexiwolf@pm.me>
2022-11-28 10:30:42 -0600

    Added Stub of `SdlContext` and `EventLoop` impl

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
-pub fn init() -> Result<sdl2::Sdl, String> {
-    sdl2::init()
+use wolf_engine::events::Event as WolfEvent;
+use wolf_engine::events::EventLoop as WolfEventLoop;
+
+pub fn init() -> Result<SdlContext, String> {
+    Err("Not implemented".to_string())
+}
+
+pub struct SdlContext {
+
+}
+
+impl WolfEventLoop<WolfEvent> for SdlContext {
+    fn next_event(&mut self) -> Option<WolfEvent> {
+        None
+    }
+
+    fn send_event(&self, event: WolfEvent) {}
 }
